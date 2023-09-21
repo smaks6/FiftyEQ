b58d3020ead0f7213d4c18b684d7af709cb90268
smaks6 <smaks2313@gmail.com>
2023-09-21 17:56:56 +0200

    fix duplication problem

--- src/main/java/me/smaks6/fiftyeq/events/DeathEvent.java
+++ src/main/java/me/smaks6/fiftyeq/events/DeathEvent.java
@@ -1,79 +1,49 @@
 package me.smaks6.fiftyeq.events;
 
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Random;
 
 public class DeathEvent implements Listener {
+    private final Random random = new Random();
 
     @EventHandler
-    public void deathEvent(PlayerDeathEvent event){
+    public void deathEvent(PlayerDeathEvent event) {
         Player player = event.getEntity();
+        PlayerInventory inventory = player.getInventory();
+        ItemStack[] contents = inventory.getContents();
+
+        int halfSize = contents.length / 2;
+
+        ItemStack[] droppedItems = new ItemStack[halfSize];
+
+        for (int i = 0; i < halfSize; i++) {
+            int randomIndex = random.nextInt(contents.length);
+            droppedItems[i] = contents[randomIndex];
+            contents[randomIndex] = null;
+        }
+        inventory.setContents(contents);
+
+        Location location = player.getLocation();
+        World world = location.getWorld();
+        for (ItemStack item : droppedItems) {
+            if (item != null && world != null) {
+                world.dropItemNaturally(location, item);
+            }
+        }
 
         event.setKeepInventory(true);
-
-        ArrayList<ItemStack> dropItems = getList(player);
-
-        dropItems = random(dropItems);
-
-        removeItem(dropItems, player);
-
-        dropItem(dropItems, player);
-
+        event.getDrops().removeAll(event.getDrops());
     }
 
-
-    private ArrayList<ItemStack> random(ArrayList<ItemStack> dropItems){
-
-        int lenght = (dropItems.size() -1) / 2;
-
-        for (int i = lenght; i > 0; i--) {
-            Random random = new Random();
-            int radnomNumber = random.nextInt(lenght);
-
-            dropItems.remove(radnomNumber);
-        }
-
-
-        return dropItems;
-    }
-
-    private void removeItem(ArrayList<ItemStack> dropitem, Player player){
-
-        for (ItemStack itemStack : dropitem) {
-
-            if((itemStack == null))
-                continue;
-
-            player.getInventory().removeItem(itemStack);
-        }
-
-
-    }
-
-    private void dropItem(ArrayList<ItemStack> dropitem, Player player){
-        for (ItemStack itemStack : dropitem) {
-
-            if((itemStack == null))
-                continue;
-
-            player.getWorld().dropItemNaturally(player.getLocation(), itemStack);
-        }
-
-    }
-
-    private ArrayList<ItemStack> getList(Player p){
-
-        ArrayList<ItemStack> list = new ArrayList<>();
-
-        for (ItemStack content : p.getInventory().getContents())
-            list.add(content);
-
-        return list;
-    }
 }
